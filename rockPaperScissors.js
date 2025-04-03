0b63d1b4be97cc94ac4263bac4e1dc6da115aa75
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-04-03 08:45:55 -0700

    Added computer choice functions

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,28 @@
 console.log("in the js file");
+
+/**
+ * Generates a number, 0-2, that is used to determine the computers choice of 
+ * rock, paper, or scissors
+ */
+function getRandomNumber () {
+    return Math.floor(Math.random() * 3);
+}
+
+function getComputerChoice () {
+    const computerNum = getRandomNumber();
+    switch (computerNum) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            console.log("Random number generator gave < 0 || > 2");
+    }
+}
+
+// Test function to output the result of various functions
+for (let i = 0; i < 5; i++) {
+    console.log(getComputerChoice());
+}
